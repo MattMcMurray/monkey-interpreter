e660a85793563263793e1660a969cf8d13cdcc62
Matt McMurray <mathieumcmurray@gmail.com>
2024-02-17 14:26:20 -0600

    Complete parsing of statments (let & return)
    End of section 2.5

--- src/token/token.go
+++ src/token/token.go
@@ -31,11 +31,13 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
